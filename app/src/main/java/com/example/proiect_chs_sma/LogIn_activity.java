60f5da6a76ad71b4b74d5eb4def6cb6d0af28f3c
ralucaborca <raluca.borca09@gmail.com>
2021-12-18 17:54:22 +0200

    Added some frontend updates for doctor and pacient!

--- app/src/main/java/com/example/proiect_chs_sma/LogIn_activity.java
+++ app/src/main/java/com/example/proiect_chs_sma/LogIn_activity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +19,9 @@ import com.google.firebase.auth.FirebaseAuth;
 public class LogIn_activity extends AppCompatActivity {
     FirebaseAuth mAuth;
     private TextView emailAddress, password;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton1;
+    private RadioButton radioButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,9 @@ public class LogIn_activity extends AppCompatActivity {
 
         emailAddress=findViewById(R.id.textnume);
         password=findViewById(R.id.textparola);
+        radioGroup=findViewById(R.id.radioGroup);
+        radioButton1=findViewById(R.id.radio_medic);
+        radioButton2=findViewById(R.id.radio_pacient);
         Button button_login = findViewById(R.id.button_login);
 
         button_login.setOnClickListener(new View.OnClickListener() {
@@ -51,5 +59,18 @@ public class LogIn_activity extends AppCompatActivity {
                 });
             }
         });
+        radioGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int u_select= radioGroup.getCheckedRadioButtonId();
+                StringBuffer result = new StringBuffer();
+                result.append("Rez ales!");
+                if(radioButton1 != null){
+                    startActivity(new Intent(LogIn_activity.this,Doctor_activity.class));
+                    result.append("\nProfesie: \n").append(radioButton1.getText().toString());
+                }
+            }
+        });
+
     }
 }
