fba16e5a2aae25c82b69c183a4c5ebdde75d376b
ralucaborca <raluca.borca09@gmail.com>
2021-12-20 11:51:21 +0200

    LogIn errors solved!

--- app/src/main/java/com/example/proiect_chs_sma/LogIn_activity.java
+++ app/src/main/java/com/example/proiect_chs_sma/LogIn_activity.java
@@ -1,5 +1,6 @@
 package com.example.proiect_chs_sma;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,9 +20,6 @@ import com.google.firebase.auth.FirebaseAuth;
 public class LogIn_activity extends AppCompatActivity {
     FirebaseAuth mAuth;
     private TextView emailAddress, password;
-    private RadioGroup radioGroup;
-    private RadioButton radioButton1;
-    private RadioButton radioButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +30,6 @@ public class LogIn_activity extends AppCompatActivity {
 
         emailAddress=findViewById(R.id.textnume);
         password=findViewById(R.id.textparola);
-        radioGroup=findViewById(R.id.radioGroup);
-        radioButton1=findViewById(R.id.radio_medic);
-        radioButton2=findViewById(R.id.radio_pacient);
         Button button_login_doctor = findViewById(R.id.button_login_doctor);
         Button button_login_pacient = findViewById(R.id.button_login_pacient);
 
@@ -45,18 +40,27 @@ public class LogIn_activity extends AppCompatActivity {
                 String passwordCreate = password.getText().toString().trim();
                 String verificareMail = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
-               /*if(!emailCreate.matches(verificareMail)){
-                    emailCreate.setError("Mail-ul este invalid!");
+                if (emailCreate.isEmpty()) {
+                    emailAddress.setError("Introduceti adresa de e-mail!");
+                    emailAddress.requestFocus();
                     return;
-                }*/
+                }else{
+               if(!emailCreate.matches(verificareMail)) {
+                   emailAddress.setError("Mail-ul este invalid!");
+                   return;
+               }
+                }
 
                 mAuth.signInWithEmailAndPassword(emailCreate, passwordCreate).addOnSuccessListener(authResult -> {
                     startActivity(new Intent(LogIn_activity.this, Doctor_activity.class));
                     finish();
                     Toast.makeText(LogIn_activity.this, "Conectare realizata cu succes!", Toast.LENGTH_SHORT).show();
-                    //public void onFailure(@NonNull Exception e){
-                    // Toast.makeText(LogIn_activity.this, e.getMessage, Toast.LENGTH_SHORT).show();
-                    //  }
+
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LogIn_activity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
                 });
             }
         });
@@ -68,33 +72,30 @@ public class LogIn_activity extends AppCompatActivity {
                 String passwordCreate = password.getText().toString().trim();
                 String verificareMail = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
-               /*if(!emailCreate.matches(verificareMail)){
-                    emailCreate.setError("Mail-ul este invalid!");
+                if (emailCreate.isEmpty()) {
+                    emailAddress.setError("Introduceti adresa de e-mail!");
+                    emailAddress.requestFocus();
                     return;
-                }*/
+                }else{
+                    if(!emailCreate.matches(verificareMail)) {
+                        emailAddress.setError("Mail-ul este invalid!");
+                        return;
+                    }
+                }
 
                 mAuth.signInWithEmailAndPassword(emailCreate, passwordCreate).addOnSuccessListener(authResult -> {
                     startActivity(new Intent(LogIn_activity.this, Pacient_activity.class));
                     finish();
                     Toast.makeText(LogIn_activity.this, "Conectare realizata cu succes!", Toast.LENGTH_SHORT).show();
-                    //public void onFailure(@NonNull Exception e){
-                    // Toast.makeText(LogIn_activity.this, e.getMessage, Toast.LENGTH_SHORT).show();
-                    //  }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LogIn_activity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
                 });
+
             }
         });
-        /*radioGroup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int u_select= radioGroup.getCheckedRadioButtonId();
-                StringBuffer result = new StringBuffer();
-                result.append("Rez ales!");
-                if(radioButton1 != null){
-                    startActivity(new Intent(LogIn_activity.this,Doctor_activity.class));
-                    result.append("\nProfesie: \n").append(radioButton1.getText().toString());
-                }
-            }
-        });*/
 
     }
 }
