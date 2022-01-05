83f3148dce84a1397899df6ebf339932192a218b
ralucaborca <raluca.borca09@gmail.com>
2022-01-05 19:26:41 +0200

    Added lista_pacienti activity!

--- app/src/main/java/com/example/proiect_chs_sma/Doctor_activity.java
+++ app/src/main/java/com/example/proiect_chs_sma/Doctor_activity.java
@@ -2,13 +2,25 @@ package com.example.proiect_chs_sma;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Doctor_activity extends AppCompatActivity {
-
+    Button lista_pacienti;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor);
+
+        lista_pacienti.findViewById(R.id.lista_pacienti);
+        lista_pacienti.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotolista = new Intent(Doctor_activity.this,ListaPacienti_activity.class);
+                startActivity(gotolista);
+            }
+        });
     }
 }
