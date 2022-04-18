a47273a2510106b04a0e075c58e81466ca050c32
ralucaborca <raluca.borca09@gmail.com>
2022-04-18 15:55:34 +0300

    Read data from firebase (doctor's feedback)!

--- app/src/main/java/com/example/proiect_chs_sma/Feedback.java
+++ app/src/main/java/com/example/proiect_chs_sma/Feedback.java
@@ -5,16 +5,19 @@ public class Feedback {
     private String nume_medic;
     private String caz_puls;
     private String feedback;
-
+    private String id_pacient;
+    private String data_ora;
     public Feedback(){
 
     }
 
-    public Feedback(String id_medic, String nume_medic, String caz_puls, String feedback){
+    public Feedback(String id_medic, String nume_medic, String caz_puls, String feedback, String id_pacient, String data_ora){
         this.id_medic = id_medic;
+        this.id_pacient = id_pacient;
         this.nume_medic = nume_medic;
         this.caz_puls = caz_puls;
         this.feedback = feedback;
+        this.data_ora = data_ora;
     }
 
     public String getID() {
@@ -23,6 +26,14 @@ public class Feedback {
 
     public void setID(String id_medic) {
         this.id_medic = id_medic;
+    }
+
+    public String getIDPacient() {
+        return id_pacient;
+    }
+
+    public void setIDPacient(String id_pacient) {
+        this.id_pacient = id_pacient;
     }
 
     public String getNume() {
@@ -47,5 +58,13 @@ public class Feedback {
 
     public void setFeedback(String feedback) {
         this.feedback = feedback;
+    }
+
+    public String getDataOra() {
+        return data_ora;
+    }
+
+    public void setDataOra(String data_ora) {
+        this.data_ora = data_ora;
     }
 }
--- app/src/main/java/com/example/proiect_chs_sma/Register_activity.java
+++ app/src/main/java/com/example/proiect_chs_sma/Register_activity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -22,6 +23,18 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
+import io.grpc.internal.JsonParser;
 
 public class Register_activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private EditText names, emails, passwords, passwords2;
@@ -98,30 +111,30 @@ public class Register_activity extends AppCompatActivity implements AdapterView.
                         return;
                     }
                 }
-                mAuth=FirebaseAuth.getInstance();
+                mAuth = FirebaseAuth.getInstance();
 
 
                 mAuth.createUserWithEmailAndPassword(emailss, passwordss).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            User user = new User(namess, emailss,"pacient");
+                        if (task.isSuccessful()) {
+                            User user = new User(namess, emailss, "pacient");
 
                             FirebaseDatabase.getInstance().getReference("Users")
                                     .child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user)
                                     .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if(task.isSuccessful()){
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
 
-                                        Toast.makeText(Register_activity.this, "User creat cu succes!",Toast.LENGTH_LONG).show();
-                                    }else{
-                                        Toast.makeText(Register_activity.this, "A aparut o eroare!",Toast.LENGTH_LONG).show();
-                                    }
-                                }
-                            });
-                        }else{
-                            Toast.makeText(Register_activity.this, "A aparut o eroare!",Toast.LENGTH_LONG).show();
+                                                Toast.makeText(Register_activity.this, "User creat cu succes!", Toast.LENGTH_LONG).show();
+                                            } else {
+                                                Toast.makeText(Register_activity.this, "A aparut o eroare!", Toast.LENGTH_LONG).show();
+                                            }
+                                        }
+                                    });
+                        } else {
+                            Toast.makeText(Register_activity.this, "A aparut o eroare!", Toast.LENGTH_LONG).show();
                         }
 
                     }
@@ -130,17 +143,50 @@ public class Register_activity extends AppCompatActivity implements AdapterView.
         });
 
 
-
         gotologin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent gotoLog= new Intent(Register_activity.this,LogIn_activity.class);
+                Intent gotoLog = new Intent(Register_activity.this, LogIn_activity.class);
                 startActivity(gotoLog);
                 finish();
             }
         });
-    }
+        /*try{
+            String result = new JsonTask().execute("https://regmed.cmr.ro/api/v1/public/cautare/"+names).get();
+            String totalResults = result.split(",")[0].replaceAll("[^0-9]", "");
+            int numberOfResults = Integer.parseInt(totalResults);
+            if(numberOfResults == 0){
+                names.requestFocus();
+                names.setError("Doctorul cu acest nume nu exista.");
+                names.setError("");
+                return;
+            }
 
+            JSONObject doctorDetails = JsonParser(result).getAsJsonObject().get("results").getAsJosnArray().get(0).getAsJsonObject();
+            String status = doctorDetails.get("status").toString().toLowerCase().replace("\"","");
+            String specialitate = doctorDetails.get("specialitati").getAsJsonArray().get(0).getAsJsonObject().get("name").toString().toLowerCase().replace("\"","");
+            if(!status.equals("activ")){
+                names.requestFocus();
+                names.setError("Doctorul nu mai profeseaza.");
+                names.setError("");
+                return;
+            }
+            if(! specialitate.equals("cardiologie")){
+                names.requestFocus();
+                names.setError("Doctorul nu este cardiolog.");
+                names.setError("");
+                return;
+            }
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    */
+    }
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String alegere = parent.getItemAtPosition(position).toString();
@@ -151,4 +197,50 @@ public class Register_activity extends AppCompatActivity implements AdapterView.
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            HttpURLConnection connection = null;
+            BufferedReader bufferedReader = null;
+            try{
+                URL url = new URL(strings[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                bufferedReader = new BufferedReader(new InputStreamReader(stream));
+                StringBuilder buffer = new StringBuilder();
+                String line = "";
+
+                while ((line = bufferedReader.readLine()) != null){
+                    buffer.append(line).append("\n");
+                }
+                return buffer.toString();
+
+
+            }catch (IOException e){
+                e.printStackTrace();
+            }finally {
+                if(connection != null){
+                    connection.disconnect();
+                }try{
+                    if(bufferedReader != null){
+                        bufferedReader.close();
+                    }
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+    }
+
 }
