33bd5f3441452f767bb233582d5b2410f48ca989
ralucaborca <raluca.borca09@gmail.com>
2022-05-09 13:28:21 +0300

    Search photo functionality added!

--- app/src/main/java/com/example/proiect_chs_sma/FormularPacient_activity.java
+++ app/src/main/java/com/example/proiect_chs_sma/FormularPacient_activity.java
@@ -1,12 +1,15 @@
 package com.example.proiect_chs_sma;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -14,6 +17,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.StorageReference;
 
 public class FormularPacient_activity extends AppCompatActivity{
     private EditText  problemes, id_pacient;
@@ -24,6 +28,12 @@ public class FormularPacient_activity extends AppCompatActivity{
     private Button button_formular, button_puls, button_imagine;
     private FirebaseUser user;
     private String userId;
+
+    Button home, rasfoieste, incarca;
+    ImageView imageView;
+    StorageReference pozaRef;
+    DatabaseReference pozafirebaase;
+    Uri imagineURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,10 +49,34 @@ public class FormularPacient_activity extends AppCompatActivity{
         pulsSpinner = (Spinner) findViewById(R.id.alegerepuls);
         fumatSpinner = (Spinner) findViewById(R.id.alegerefumat);
         sportSpinner = (Spinner) findViewById(R.id.alegeresport);
+
+        rasfoieste = findViewById(R.id.rasfoieste_btn);
+        incarca = findViewById(R.id.uploadphoto_btn);
+        imageView = findViewById(R.id.fotoView);
         pacients = new Pacients();
 
         mDatabase = FirebaseDatabase.getInstance();
         databaseReference = mDatabase.getReference("Despre pacienti");
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openPhoto = new Intent();
+                openPhoto.setAction(Intent.ACTION_GET_CONTENT);
+                openPhoto.setType("image/*");
+                startActivityForResult(openPhoto, 2);
+            }
+        });
+        rasfoieste.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openPhoto = new Intent();
+                openPhoto.setAction(Intent.ACTION_GET_CONTENT);
+                openPhoto.setType("image/*");
+                startActivityForResult(openPhoto, 2);
+            }
+        });
+
 
         button_formular.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,7 +98,13 @@ public class FormularPacient_activity extends AppCompatActivity{
                 pacients.setFumat(fumat);
                 pacients.setSport(sport);
                 pacients.setSanatate(probleme_sanatate);
+
                 pacients.setIdPacient(idpacient);
+                    if(imagineURI != null){
+                       // uploadToFirebase(imagineURI);
+                    }else{
+                        Toast.makeText(FormularPacient_activity.this, "Va rugam incarcati imaginea cu pulsul dvs.",Toast.LENGTH_SHORT).show();
+                    }
 
                 databaseReference.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(pacients);
                 Toast.makeText(FormularPacient_activity.this, "Informatiile pacientului au fost adaugate cu succes!",
@@ -89,6 +129,12 @@ public class FormularPacient_activity extends AppCompatActivity{
                 startActivity(goImage);
             }
         });
-
+    }
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 2 && resultCode == RESULT_OK && data != null){
+            imagineURI = data.getData();
+            imageView.setImageURI(imagineURI);
+        }
     }
 }
